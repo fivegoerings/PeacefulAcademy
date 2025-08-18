935dc9037c212d38b08d7b9e5c87d4d9bdce9a24
fivegoerings <fivegoerings@gmail.com>
2025-08-18 00:05:48 -0500

    remove auth fix deploy issues

--- netlify/functions/auth-logout.js
+++
@@ -1,9 +0,0 @@
-import { clearSessionCookie } from './_shared/auth.js';
-
-export default async () => {
-  return new Response(JSON.stringify({ ok:true }), {
-    status: 200,
-    headers: { 'set-cookie': clearSessionCookie(), 'content-type':'application/json' }
-  });
-};
-
